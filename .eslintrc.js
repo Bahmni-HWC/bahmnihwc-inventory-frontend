279b87e444373bca49643d7d7e25fe3e243b7932
Abinaya U <77735030+abinaya-u@users.noreply.github.com>
2023-06-16 08:48:04 +0530

    add. workflow to build and publish bahmni-inventory image (#2)
    * add. pull request workflow
    Co-authored-by: deeptirawat1510 <deepti.rawat@thoughtworks.com>
    
    * add. build-publish workflow
    Co-authored-by: deeptirawat1510 <deepti.rawat@thoughtworks.com>
    
    * fix. PR comments
    Co-authored-by: deeptirawat1510 <deepti.rawat@thoughtworks.com>

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:react/recommended', 'prettier'],
+  globals: {
+    BASE_NAME: 'readonly',
+    BACKEND_BASE_URL: 'readonly',
+    BACKEND_API_PATH: 'readonly',
+    DICOM_BASE_URL: 'readonly',
+    DICOM_SERVER_PATH: 'readonly',
+    REACT_APP_SITE_TITLE: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/*.test.js', '**/*.test.jsx','**/*.spec.js', '**/*.spec.jsx']}],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/setupTests.js', '**/*.test.js'] },
+    ],
+  },
+};
