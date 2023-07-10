7ce4b190e242b80f69d12025a9171902ad734071
Abinaya U <abinaya.u@thoughtworks.com>
2023-07-10 19:54:03 +0530

    add. functionality to filter and sort based on patientID

--- src/inventory/dispense/dispense-page.jsx
+++ src/inventory/dispense/dispense-page.jsx
@@ -11,16 +11,20 @@ import {
 	TableToolbar,
 	TableToolbarContent,
 	TableToolbarSearch,
+	Loading,
 } from "carbon-components-react";
 import useSWR from "swr";
-import { fetcher, invItemURL, activePatientWithDrugOrders } from "../../utils/api-utils";
-import {locationCookieName, dispenseHeaders, activePatients } from "../../../constants";
+import { fetcher, activePatientWithDrugOrders } from "../../utils/api-utils";
+import {
+	locationCookieName,
+	dispenseHeaders,
+	activePatients,
+} from "../../../constants";
 import { useCookies } from "react-cookie";
-
 
 export const DispensePage = () => {
 	let rows = [];
-    const [cookies] = useCookies();
+	const [cookies] = useCookies();
 	const { data: items, error: inventoryItemError } = useSWR(
 		activePatientWithDrugOrders(cookies[locationCookieName]?.uuid),
 		fetcher
@@ -32,52 +36,43 @@ export const DispensePage = () => {
 		setSearchText(event.target.value);
 	};
 
-    if(Array.isArray(items)) {
-        rows = items.map((item) => {
-            return {
-                item,
-                id:item.uuid,
-                patientName: item.name,
-                patientId: item.identifier,
+	if (Array.isArray(items)) {
+		rows = items.map((item) => {
+			return {
+				item,
+				id: item.uuid,
+				patientName: item.name,
+				patientId: item.identifier,
+			};
+		});
+	}
 
-            }
-        })
-
-    }
-    console.log("item...", items);
 	const filteredRows = rows.filter((row) => {
-		console.log(
-			"first",
-			row?.patientName,
-			searchText,
-			row?.patientName?.toLowerCase().includes(searchText?.toLowerCase())
-		);
 		return searchText !== ""
-			? row?.patientName?.toLowerCase().includes(searchText?.toLowerCase())
+			? row?.patientName?.toLowerCase().includes(searchText?.toLowerCase()) ||
+					row?.patientId?.toLowerCase().includes(searchText?.toLowerCase())
 			: row;
 	});
 
+	const handleRowClick = (row) => {
+		console.log("row click ", row);
+	};
 
-	const isSortable = (key) => key === "patientName";
-
-    const handleRowClick = (row) => {
-        console.log("row click ", row);
-    }
-
-	if (items == undefined && inventoryItemError == undefined)
-		return <div>Loading...</div>;
+	if (items == undefined && inventoryItemError == undefined) return <Loading />;
 
 	return inventoryItemError ? (
 		<div>Something went wrong while fetching items</div>
 	) : (
-
-		<div className="inv-datatable" style={{width:'50%'}}>
-		<h5 style={{paddingBottom:'1rem'}}>{activePatients}</h5>
-			<DataTable rows={filteredRows} headers={dispenseHeaders} stickyHeader={true}>
+		<div className="inv-datatable" style={{ width: "50%" }}>
+			<h5 style={{ paddingBottom: "1rem" }}>{activePatients}</h5>
+			<DataTable
+				rows={filteredRows}
+				headers={dispenseHeaders}
+				stickyHeader={true}
+			>
 				{({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
-					<>
-						<TableContainer >
-						<TableToolbar style={{ width: "15rem"}}  >
+					<TableContainer>
+						<TableToolbar style={{ width: "15rem" }}>
 							<TableToolbarContent style={{ justifyContent: "flex-start" }}>
 								<TableToolbarSearch
 									value={searchText}
@@ -85,14 +80,14 @@ export const DispensePage = () => {
 								/>
 							</TableToolbarContent>
 						</TableToolbar>
-						<Table {...getTableProps()} >
+						<Table {...getTableProps()}>
 							<TableHead>
-								<TableRow >
+								<TableRow>
 									{headers.map((header) => (
 										<TableHeader
 											{...getHeaderProps({
 												header,
-												isSortable: isSortable(header.key),
+												isSortable: true,
 											})}
 										>
 											{header.header}
@@ -102,19 +97,25 @@ export const DispensePage = () => {
 							</TableHead>
 							<TableBody>
 								{rows.map((row) => (
-									<TableRow {...getRowProps({ row })} onClick={()=>handleRowClick(row)}>
+									<TableRow
+										{...getRowProps({ row })}
+										onClick={() => handleRowClick(row)}
+									>
 										{row.cells.map((cell) => (
-											<TableCell key={cell.id}> {
-											cell.info.header === "patientName" ?
-											<a href="#">{cell.value}</a>:cell.value}</TableCell>
+											<TableCell key={cell.id}>
+												{" "}
+												{cell.info.header === "patientName" ? (
+													<a href="#">{cell.value}</a>
+												) : (
+													cell.value
+												)}
+											</TableCell>
 										))}
-
 									</TableRow>
 								))}
 							</TableBody>
 						</Table>
-						</TableContainer>
-					</>
+					</TableContainer>
 				)}
 			</DataTable>
 		</div>
