bfe16f11e0c893285a2920fa05a059d2918243aa
Abinaya U <abinaya.u@thoughtworks.com>
2023-07-27 21:58:23 +0530

    GOK-28 | fix. lint issue

--- src/inventory/dispense/drug-mapper.js
+++ src/inventory/dispense/drug-mapper.js
@@ -1,20 +1,15 @@
-export const getMappedDrugs = (visitDrugOrder) => {
-	return {
-		id: visitDrugOrder.uuid,
-		drugName: visitDrugOrder.drug.name,
-		drugUuid: visitDrugOrder.drug.uuid,
-		quantity: visitDrugOrder.dosingInstructions.quantity,
-		dosingInstructions: visitDrugOrder.dosingInstructions,
-		duration: visitDrugOrder.duration,
-		durationUnits: visitDrugOrder.durationUnits,
-	};
-};
+export const getMappedDrugs = (visitDrugOrder) => ({
+	id: visitDrugOrder.uuid,
+	drugName: visitDrugOrder.drug.name,
+	drugUuid: visitDrugOrder.drug.uuid,
+	quantity: visitDrugOrder.dosingInstructions.quantity,
+	dosingInstructions: visitDrugOrder.dosingInstructions,
+	duration: visitDrugOrder.duration,
+	durationUnits: visitDrugOrder.durationUnits,
+});
 
-
-export const getDrugItems = (patient, prescribedDrugs)=>{
- return {
-    id: patient.id,
-    name: patient.name,
-    dispense_drugs: [...prescribedDrugs],
-  }
-}
+export const getDrugItems = (patient, prescribedDrugs) => ({
+	id: patient.id,
+	name: patient.name,
+	dispense_drugs: [...prescribedDrugs],
+});
