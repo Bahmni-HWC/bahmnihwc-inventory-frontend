34d13563c3d6f1dae221aac82e88cc18516d9049
deepthi-m <deepthi.mantena@thoughtworks.com>
2023-07-27 10:40:10 +0530

    Deepthi M| GOK-211 |updated comments for the formatted date function

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -1,3 +1,5 @@
+// Function to get the current date and time in a formatted string.
+// The returned date format will be "dd-MM-yyyy HH:mm:ss".
 const getFormattedDate = () => {
    const currentDate = new Date();
     return `${currentDate.getDate().toString().padStart(2, '0')}-${
