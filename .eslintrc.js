0a1db8c7892f92f08779a389689d601f56ed081d
JagadeeshM07 <90327651+JagadeeshM07@users.noreply.github.com>
2023-08-07 09:10:11 +0530

    #260 | Jagadeesh | Add. UI Fixes for add new drug modal (#20)
    * #260 | Jagadeesh | Add. UI Fixes for add new drug modal
    
    * #260 | Jagadeesh | Fix lint issues
    
    * #260 | Jagadeesh | Add. overwriting styles to index.scss
    
    * #260 | Jagadeesh | Remove. overwriting styles of stock receipt modal footer buttons

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,12 +24,22 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/*.test.js', '**/*.test.jsx','**/*.spec.js', '**/*.spec.jsx']}],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx'] },
+    ],
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/no-extraneous-dependencies': [
       'error',
       { devDependencies: ['**/setupTests.js', '**/*.test.js'] },
     ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { CookiesProvider } from "react-cookie";
-import InventoryMenu from "./inventory/inventory-menu.jsx";
-import InventoryHeader from "./components/header/header.jsx";
-import "../index.scss";
-import { ItemStockProvider } from "./context/item-stock-context.jsx";
+import React from 'react';
+import { CookiesProvider } from 'react-cookie';
+import InventoryMenu from './inventory/inventory-menu';
+import InventoryHeader from './components/header/header';
+import '../index.scss';
+import { ItemStockProvider } from './context/item-stock-context';
 
 const App = () => (
-	<CookiesProvider>
-		<InventoryHeader />
-		<div style={{ paddingTop: "2rem", paddingLeft: "1rem" }}>
-			<ItemStockProvider>
-				<InventoryMenu />
-			</ItemStockProvider>
-		</div>
-	</CookiesProvider>
+  <CookiesProvider>
+    <InventoryHeader />
+    <div style={{ paddingTop: '2rem', paddingLeft: '1rem' }}>
+      <ItemStockProvider>
+        <InventoryMenu />
+      </ItemStockProvider>
+    </div>
+  </CookiesProvider>
 );
 
 export default App;
