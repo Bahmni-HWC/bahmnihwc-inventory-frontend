54b6639da3c5c56a6d15bf12f86d58ba2942ca29
Jagadeesh Mohan <jagadeesh@Jagadeeshs-MacBook-Pro.local>
2023-08-09 17:11:01 +0530

    #293 | Jagadeesh | UI fixed for stock details table in the modal of inventory page

--- src/inventory/inventory-menu.jsx
+++ src/inventory/inventory-menu.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Tabs, Tab, Loading } from 'carbon-components-react';
 import { useCookies } from 'react-cookie';
 import useSWR from 'swr';
-import { InventoryLandingPage } from './inventory-landing-page';
+import InventoryLandingPage from './inventory-landing-page';
 import { getLocationName, inventoryMenu, locationCookieName } from '../../constants';
 import DispensePage from './dispense/dispense-page';
 import StockReceipt from './stock-receipt/stock-receipt';
@@ -11,59 +11,59 @@ import { useItemStockContext, useStockRoomContext } from '../context/item-stock-
 import { ResponseNotification } from '../components/notifications/response-notification';
 
 const InventoryMenu = () => {
-	const [cookies] = useCookies();
-	const { setItemStock, setItemStockError } = useItemStockContext();
-	const { setStockRoom, setStockRoomError } = useStockRoomContext();
+  const [cookies] = useCookies();
+  const { setItemStock, setItemStockError } = useItemStockContext();
+  const { setStockRoom, setStockRoomError } = useStockRoomContext();
 
-	const { data: stockRoom, error: stockRoomError } = useSWR(
-		stockRoomURL(cookies[locationCookieName]?.name.trim()),
-		fetcher
-	);
+  const { data: stockRoom, error: stockRoomError } = useSWR(
+    stockRoomURL(cookies[locationCookieName]?.name.trim()),
+    fetcher,
+  );
 
-	const { data: items, error: inventoryItemError } = useSWR(
-		stockRoom ? invItemURLByStockroom(stockRoom.results[0].uuid) : '',
-		fetcher
-	);
+  const { data: items, error: inventoryItemError } = useSWR(
+    stockRoom ? invItemURLByStockroom(stockRoom.results[0].uuid) : '',
+    fetcher,
+  );
 
-	useEffect(() => {
-		if (items) {
-			setItemStock(items.results);
-		}
-		if (inventoryItemError) {
-			setItemStockError(inventoryItemError);
-		}
-	}, [items, inventoryItemError]);
+  useEffect(() => {
+    if (items) {
+      setItemStock(items.results);
+    }
+    if (inventoryItemError) {
+      setItemStockError(inventoryItemError);
+    }
+  }, [items, inventoryItemError]);
 
-	useEffect(() => {
-		if (stockRoom) {
-			setStockRoom(stockRoom.results);
-		}
-		if (stockRoomError) {
-			setStockRoomError(stockRoomError);
-		}
-	}, [stockRoom, stockRoomError]);
+  useEffect(() => {
+    if (stockRoom) {
+      setStockRoom(stockRoom.results);
+    }
+    if (stockRoomError) {
+      setStockRoomError(stockRoomError);
+    }
+  }, [stockRoom, stockRoomError]);
 
-	if ((items === undefined && inventoryItemError === undefined) || (!stockRoom && !stockRoomError))
-		return <Loading />;
+  if ((items === undefined && inventoryItemError === undefined) || (!stockRoom && !stockRoomError))
+    return <Loading />;
 
-	return inventoryItemError ? (
-		<div>{ResponseNotification('error', 'Error', 'Something went wrong while fetching URL')}</div>
-	) : (
-		<div style={{ paddingTop: '2rem' }}>
-			<h4>{getLocationName(cookies[locationCookieName]?.name)} </h4>
-			<Tabs>
-				<Tab label={inventoryMenu[0]}>
-					<InventoryLandingPage />
-				</Tab>
-				<Tab label='Stock Receipt'>
-					<StockReceipt />
-				</Tab>
-				<Tab label={inventoryMenu[1]}>
-					<DispensePage />
-				</Tab>
-			</Tabs>
-		</div>
-	);
+  return inventoryItemError ? (
+    <div>{ResponseNotification('error', 'Error', 'Something went wrong while fetching URL')}</div>
+  ) : (
+    <div style={{ paddingTop: '2rem' }}>
+      <h4>{getLocationName(cookies[locationCookieName]?.name)} </h4>
+      <Tabs>
+        <Tab label={inventoryMenu[0]}>
+          <InventoryLandingPage />
+        </Tab>
+        <Tab label="Stock Receipt">
+          <StockReceipt />
+        </Tab>
+        <Tab label={inventoryMenu[1]}>
+          <DispensePage />
+        </Tab>
+      </Tabs>
+    </div>
+  );
 };
 
 export default InventoryMenu;
