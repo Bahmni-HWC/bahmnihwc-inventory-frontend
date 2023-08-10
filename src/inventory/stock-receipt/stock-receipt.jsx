ddfa81ce95eef76cafaf6c99dac075e2848b26b4
yenugukeerthana <keerthana.yenugu@thoughtworks.com>
2023-08-10 11:18:52 +0530

    Update. Invalid message for negative numbers in load stock screen total quantity
    Co-authored-by: deeptirawat1510 <deepti.rawat@thoughtworks.com>

--- src/inventory/stock-receipt/stock-receipt.jsx
+++ src/inventory/stock-receipt/stock-receipt.jsx
@@ -241,7 +241,7 @@ const StockReceipt = () => {
   };
 
   const handleInputChange = (id, field, value) => {
-    (field === 'totalQuantity' && value < 0)? setNegativeError(true): setNegativeError(false);
+    (field === 'totalQuantity')?((value < 0)?setNegativeError(true):setNegativeError(false)):"";
     setRows((prevRows) =>
       prevRows.map((row) => (row.id === id ? { ...row, [field]: value } : row)),
     );
