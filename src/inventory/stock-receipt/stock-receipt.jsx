8418c491e16dafd16226ee0827ddcf30dced0c51
Abinaya U <abinaya.u@thoughtworks.com>
2023-08-11 12:41:11 +0530

    fix. auto reload of inventory items Co-authored-by: deepthi-m <deepthi.mantena@thoughtworks.com>

--- src/inventory/stock-receipt/stock-receipt.jsx
+++ src/inventory/stock-receipt/stock-receipt.jsx
@@ -296,7 +296,7 @@ const StockReceipt = (props) => {
 
   const isInvalid = (id) => {
     const row = rows.find((row) => row.id === id);
-    return row?row.invalid:false;
+    return row ? row.invalid : false;
   };
 
   const handleInputChange = (id, field, value) => {
@@ -499,7 +499,8 @@ const StockReceipt = (props) => {
               {ResponseNotification(
                 'info',
                 'info',
-                'No data is received for the outward number. Could you please retry?',setStockEmptyResonseMessage
+                'No data is received for the outward number. Could you please retry?',
+                setStockEmptyResonseMessage,
               )}
             </div>
           )}
