7b84ea37509aca2b8ce7856fcb355a9a93506b26
yenugukeerthana <keerthana.yenugu@thoughtworks.com>
2023-10-04 14:15:06 +0530

    Refactor. Code
    Co-authored-by: deeptirawat1510 <deepti.rawat@thoughtworks.com>

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -8,9 +8,23 @@ const getFormattedDate = () => {
 
 };
 
+const convertToDateTimeFormat = (inputTime) => {
+  const dateObject = new Date(inputTime);
+  const year = dateObject.getFullYear();
+  const month = String(dateObject.getMonth() + 1).padStart(2, '0');
+  const day = String(dateObject.getDate()).padStart(2, '0');
+  const hours = String(dateObject.getHours()).padStart(2, '0');
+  const minutes = String(dateObject.getMinutes()).padStart(2, '0');
+  const seconds = String(dateObject.getSeconds()).padStart(2, '0');
+  const milliseconds = String(dateObject.getMilliseconds()).padStart(3, '0');
+  const timezoneOffset = '0000';
+
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}+${timezoneOffset}`;
+};
+
 const getDatePattern = () => {
   const datePattern = "(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/\\d{4}"
   return datePattern;
 };
 
-export { getFormattedDate, getDatePattern };
+export { getFormattedDate,convertToDateTimeFormat, getDatePattern };
