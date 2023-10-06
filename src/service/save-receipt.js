3a2ee72756baab700f65ab4501680074fdcf3624
yenugukeerthana <keerthana.yenugu@thoughtworks.com>
2023-10-06 14:40:15 +0530

    Add. Inward API functionality for stock receipt
    Co-authored-by: deeptirawat1510 <deepti.rawat@thoughtworks.com>

--- src/service/save-receipt.js
+++ src/service/save-receipt.js
@@ -67,25 +67,7 @@ const inwardSaveReceipt = async (items, institutionId, inwardDate, destinationUu
   const requestBody = {
     status: 'NEW',
     attributes: [],
-    // items: itemsArray,
-   items: [
-      {
-          item: "37d4b9c8-635c-4de8-8929-805d3101232b",
-          quantity: 2,
-          expiration: "30-11-2024",
-          batchNumber: "A361",
-          calculatedExpiration: true,
-          inwardNumber: "INW-0000004"
-      },
-      {
-        item: "3093c9da-b362-473f-b699-05e7fdb52506",
-        quantity: 5,
-        expiration: "31-07-2025",
-        batchNumber: "EM981",
-        calculatedExpiration: true,
-        inwardNumber: "INW-0000005"
-      }
-    ],
+    items: itemsArray,
     operationNumber: '',
     instanceType: instanceTypeUuids,
     operationDate: getFormattedDate(),
